e7bad86681f5ce2a35a40b9ca997f007b82c5b38
anonsl <87575544+anonsl@users.noreply.github.com>
2021-07-20 12:51:40 +0200

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -1,18 +1,18 @@
 import time
-import pyautogui
+import pyautogui # neccecary library for keyboard/mouse integration in Python3.xx
 
-
-pyautogui.FAILSAFE = True
+pyautogui.FAILSAFE = True # neccecary to exit the program.
 
 def main():
     start = input(str("Start autoclicker? y/N? "))
     if start == "y":
         print("sleeping for 10 seconds")
         time.sleep(10)
-        print(time.asctime)
         while True:
-            for i in range(0,1):
-                pyautogui.moveTo(1143,373)
-                pyautogui.doubleClick(1143,373)
-                pyautogui.doubleClick(1143,373)
-main()
+            pyautogui.moveTo(1143,373) # pixels of the "flirt" button, screen resolution 1920*1080
+            pyautogui.doubleClick(1143,373) # double clicks the "flirt" button.
+    else:
+        return 1 # standard C output for IO error.
+        exit()
+    
+main() # run main function.    
